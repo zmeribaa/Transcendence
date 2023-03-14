3bff1129a13475ef1414b1d19dd73e986da31b8c
Ziyad Meribaa <zmeribaa@e2r1p4.1337.ma>
2023-03-14 15:12:30 +0100

    Game is Functional

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,15 @@ const PongGame: React.FC = () => {
 		width: number;
 		height: number;
 		speed: number;
+		top: number;
+		bottom: number;
+		left: number;
+		right: number;
+	}
+
+	interface score {
+		player1: number;
+		player2: number;
 	}
 	interface Ball {
 		x: number;
@@ -18,13 +27,27 @@ const PongGame: React.FC = () => {
 		speed: number;
 		velocityX: number;
 		velocityY: number;
+		top: number;
+		bottom: number;
+		left: number;
+		right: number;
 	}
+
+	const [score, setscore] = useState<score>({
+		player1: 0,
+		player2: 0,
+	});
+
 	const [paddle1, setpaddle1] = useState<Paddle>({
 		x: 10,
 		y: 20,
 		width: 20,
 		height: 100,
 		speed: 5,
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0,
 	});
 	const [paddle2, setpaddle2] = useState<Paddle>({
 		x: 1370,
@@ -32,6 +55,10 @@ const PongGame: React.FC = () => {
 		width: 20,
 		height: 100,
 		speed: 5,
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0,
 	});
 	const [ball, setball] = useState<Ball>({
 		x: cvsWidth / 2,
@@ -39,13 +66,69 @@ const PongGame: React.FC = () => {
 		speed: 5,
 		velocityX: 5,
 		velocityY: 5,
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0,
 	});
 	
   const setup:any = (p5: p5Types, canvasParentRef: Element) => {
     p5.createCanvas(cvsWidth, cvsHeight).parent(canvasParentRef);
   };
+  function collision(ball: Ball, player: Paddle): boolean {
+	let ballRight: number = ball.x + ball.speed;
+	let ballBottom: number = ball.y + ball.speed;
+	let playerRight: number = player.x + 10;
+	let playerTop: number = player.y;
+	let playerBottom: number = player.y + player.height;
+  
+	return (
+	  ballRight >= player.x &&
+	  ball.x <= playerRight &&
+	  ball.y >= playerTop &&
+	  ballBottom <= playerBottom
+	);
+  }
+  
+  function update(): void {
+	ball.x += ball.velocityX;
+	ball.y += ball.velocityY;
+  
+	if (ball.y + ball.speed > cvsHeight || ball.y - ball.speed < 0) {
+	  ball.velocityY = -ball.velocityY;
+	}
+  
+	let player: Paddle = ball.x < cvsWidth / 2 ? paddle1 : paddle2;
+  
+	if (collision(ball, player)) {
+	  let collidePoint: number = ball.y - (player.y + player.height / 2);
+	  collidePoint = collidePoint / (player.height / 2);
+  
+	  let angleRad: number = (Math.PI / 4) * collidePoint;
+	  let direction: number = ball.x < cvsWidth / 2 ? 1 : -1;
+  
+	  ball.velocityX = direction * ball.speed * Math.cos(angleRad);
+	  ball.velocityY = ball.speed * Math.sin(angleRad);
+	  ball.speed += 0.1;
+	}
+	if (ball.x - ball.speed < 0) {
+	  setscore(prevScore => ({ ...prevScore, player2: prevScore.player2 + 1 }));
+	  resetBall();
+	} else if (ball.x + ball.speed > cvsWidth) {
+	  setscore(prevScore => ({ ...prevScore, player1: prevScore.player1 + 1 }));
+	  resetBall();
+  }
+}
 
+  function resetBall(): void {
+	ball.x = cvsWidth / 2;
+	ball.y = cvsHeight / 2;
+	ball.speed = 5;
+	ball.velocityX = -ball.velocityX;
+	ball.velocityY = -ball.velocityY;
+	  }
   const draw: any = (p5: p5Types) => {
+	  update();
     p5.background(97, 72, 217);
     p5.noStroke();
     p5.rect(paddle1.x, paddle1.y, 20, 100);
@@ -73,14 +156,16 @@ const PongGame: React.FC = () => {
 	}
 
 	// draw the ball
-
+	p5.fill(255);
+	p5.ellipse(ball.x, ball.y, 20, 20);
 	
 
 	// draw the score
-	p5.fill(255);
-	p5.textSize(32);
-	p5.text('0', 600, 50);
-	p5.text('0', 800, 50);
+	p5.stroke(255);
+	p5.strokeWeight(2);
+	p5.textSize(50);
+	p5.text(score.player1, 300, 100);
+	p5.text(score.player2, 900, 100);
 
   };
 
